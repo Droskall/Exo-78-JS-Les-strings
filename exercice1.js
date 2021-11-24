d68cc7761a8ee476f1ada023ef0641d4b5e9c1bc
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-11-24 16:29:44 +0100

    finish

--- exercice1.js
+++ exercice1.js
@@ -2,11 +2,27 @@ let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
 
 // Completer le code suivant pour afficher la longueur de la variable monTexte
 
-alert(monTexte.);
+alert(monTexte.length);
 
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
 
+let result = monTexte.indexOf('i');
+
+let position = document.getElementById('position');
+position.innerHTML = result;
+
+let majText = monTexte.toUpperCase();
+
+let maj = document.getElementById('chaineMaj');
+maj.innerHTML = majText;
+
+for (let i = 0; i < monTexte.length; i++) {
+    if (monTexte[i] === "a") {
+        alert("lettre a trouvé");
+        i++
+    }
+}
 
 
 
